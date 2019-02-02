9a306dbf74ac4b46acf1b867c693b4fa6e618fb3
Douglas Swiatocha <dswiatocha@gmail.com>
2019-02-02 14:13:24 -0600

    mostly finished

--- main.py
+++ main.py
@@ -4,24 +4,65 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['POST'])
-def add_user():
-    new_user = request.form['new-user']
-
-@app.route("/", methods=['POST'])
-def password():
-    pswd = request.form['set-pswd']
-
-@app.route("/", methods=['POST'])
-def verify_password():
-    verify_pswd = request.form['verify-pswd']
-
-@app.route("/", methods=['POST'])
-def email():
-    set_email = request.form['set-email']
-
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', new_user="", user_error="")
+
+@app.route("/", methods=['POST'])
+def add_user():
+
+    new_user = request.form['new-user']
+    user_error = ""
+    if len(new_user) < 3:
+        user_error = "User name must be greater than two characters."
+        new_user = ""
+    elif len(new_user) > 20:
+        user_error = "User name must be less than twenty characters."
+        new_user = ""
+    for char in new_user:
+        if char == " ":
+            user_error = "User name cannot contain spaces."
+            new_user = ""
+
+    pswd = request.form['set-pswd']
+    password_error = ""
+    if len(pswd) < 3:
+        password_error = "Password must be greater than two characters."
+        pswd = ""
+    elif len(pswd) > 20:
+        password_error = "Password must be less than twenty characters."
+        pswd = ""
+    for char in pswd:
+        if char == " ":
+            password_error = "Password cannot contain spaces."
+            pswd = ""
+
+    verify_pswd = request.form['verify-pswd']
+    verify_error = ""
+    if verify_pswd != pswd:
+        verify_error = "Password does not match."
+        verify_pswd = ""
+
+    set_email = request.form['new-email']
+    email_error = ""
+    if "@" not in set_email:
+        email_error = "Email must contain '@' symbol."
+        set_email = ""
+    elif "." not in set_email:
+        email_error = "Email must contain '.' symbol."
+        set_email = ""
+    
+    if not user_error:
+        if not password_error:
+            if not verify_error:
+                if not email_error:
+                    return render_template('welcome.html', new_user=new_user)
+                else: return render_template('index.html', new_user=new_user, set_email=set_email, user_error=user_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
+            else:
+                return render_template('index.html', new_user=new_user, set_email=set_email, user_error=user_error, password_error=password_error, verify_error=verify_error)
+        else:
+            return render_template('index.html', new_user=new_user, set_email=set_email, user_error=user_error, password_error=password_error)
+    else:
+        return render_template('index.html', new_user=new_user, set_email=set_email, user_error=user_error)
 
 app.run()
